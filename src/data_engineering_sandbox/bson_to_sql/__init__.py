7e76c387ea814de90880a4665ff41f82a0c940f4
Anaëlle Laurans <16943368+Laurans@users.noreply.github.com>
2023-09-01 07:13:25 +0200

    💡 add docstrings / comments in code

--- src/data_engineering_sandbox/bson_to_sql/__init__.py
+++ src/data_engineering_sandbox/bson_to_sql/__init__.py
@@ -29,6 +29,11 @@ from ._transformations import NestedDataTransformer, normalize_id
 
 
 class CustomTypeEncoder(JSONEncoder):
+    """JSON decoder for bson files.
+
+    Some types need additionnal decoding to be compatible with sqlalchemy table definition.
+    """
+
     def default(self, o: Any) -> Any:
         if isinstance(o, datetime.datetime):
             return o.isoformat()
@@ -42,7 +47,27 @@ class CustomTypeEncoder(JSONEncoder):
 
 def _load_record(
     session: Session, content: dict, table_definition: "DeclarativeAttributeIntercept"
-):
+) -> str | id:
+    """Load a datapoint in the database and return the inserted id.
+
+    Parameters
+    ----------
+    session
+        sqlalchemy session
+    content
+        datapoint to insert in dict format
+    table_definition
+        sqlalchemy table definition
+
+    Returns
+    -------
+        inserted id
+
+    Raises
+    ------
+    err
+        Exception when something goes wrong
+    """
     try:
         datapoint = table_definition(**content)
         session.add(datapoint)  # TODO: look if we can do a on conflict to nothing
@@ -50,20 +75,19 @@ def _load_record(
         session.commit()
     except sqlalchemy.exc.IntegrityError:
         pass
-    except sqlalchemy.exc.DataError as e:
-        if type(e.orig) == psycopg2.errors.InvalidTextRepresentation:
+    except sqlalchemy.exc.DataError as err:
+        if type(err.orig) == psycopg2.errors.InvalidTextRepresentation:
             pass
-    except sqlalchemy.exc.PendingRollbackError as e:
+    except sqlalchemy.exc.PendingRollbackError as err:
         if (
-            "psycopg2.errors.InvalidTextRepresentation" in e.args[0]
-            or "psycopg2.errors.UniqueViolation" in e.args[0]
+            "psycopg2.errors.InvalidTextRepresentation" in err.args[0]
+            or "psycopg2.errors.UniqueViolation" in err.args[0]
         ):
             pass
         else:
-            raise e
-    except Exception as e:
-        breakpoint()
-        raise e
+            raise err
+    except Exception as err:
+        raise err
     return datapoint.id
 
 
@@ -71,7 +95,22 @@ def _load_nested_table(
     session: Session,
     content: dict,
     nested_table_definition: dict[str, NestedDataTransformer] = {},
-):
+) -> dict:
+    """load nested data points that goes in another table in the database
+
+    Parameters
+    ----------
+    session
+        sqlalchemy session
+    content
+        actual data as a dictionary from which to extract the nested data we want in another table
+    nested_table_definition, optional
+        dictionary where the key correspond to the key to extract from content, by default {}
+
+    Returns
+    -------
+        data without the nested data defined by the key in nested_table_definintion
+    """
     for key, record_transformation in nested_table_definition.items():
         if key in content.keys() and isinstance(content[key], list):
             [
@@ -109,6 +148,19 @@ def _load_bson_file_in_postgres(
     engine: "Engine",
     nested_table_definition: dict[str, NestedDataTransformer] = {},
 ):
+    """Ingest a table postgres with records.
+
+    Parameters
+    ----------
+    data_file
+        source file of records
+    table_definition
+        main table definition rattached to the data file
+    engine
+        sqlalchemy engine
+    nested_table_definition, optional
+        dictionary representing nested data that goes in another tables, by default {}
+    """
     with Session(engine) as session:
         session.rollback()
         with open(data_file, "r") as fp:
@@ -126,6 +178,7 @@ def _load_bson_file_in_postgres(
 
 
 def load_mflix_files(directory: "Path", engine: "Engine"):
+    """Load sample_mflix in postgres."""
     mflix.create_tables(engine)
     for data_file in directory.iterdir():
         match data_file.stem:
@@ -144,6 +197,7 @@ def load_mflix_files(directory: "Path", engine: "Engine"):
 
 
 def load_geospatial_files(directory: "Path", engine: "Engine"):
+    """Load sample_geospatial in postgres."""
     geospatial.create_tables(engine)
     for data_file in directory.iterdir():
         match data_file.stem:
@@ -154,6 +208,7 @@ def load_geospatial_files(directory: "Path", engine: "Engine"):
 
 
 def load_airbnb_files(directory: "Path", engine: "Engine"):
+    """Load sample_airbnb in postgres."""
     airbnb.create_tables(engine)
     for data_file in directory.iterdir():
         match data_file.stem:
@@ -177,6 +232,7 @@ def load_airbnb_files(directory: "Path", engine: "Engine"):
 
 
 def load_analytics_files(directory: "Path", engine: "Engine"):
+    """Load sample_analytics in postgres."""
     analytics.create_tables(engine)
     nested_table_definitions = {}
     for data_file in directory.iterdir():
@@ -210,6 +266,7 @@ def load_analytics_files(directory: "Path", engine: "Engine"):
 
 
 def load_supplies_files(directory: "Path", engine: "Engine"):
+    """Load sample_supplies in postgres."""
     supplies.create_tables(engine)
     nested_table_definitions = {}
     for data_file in directory.iterdir():
@@ -231,6 +288,7 @@ def load_supplies_files(directory: "Path", engine: "Engine"):
 
 
 def load_weatherdata_files(directory: "Path", engine: "Engine"):
+    """Load sample_weatherdata in postgres."""
     weatherdata.create_tables(engine)
     for data_file in directory.iterdir():
         match data_file.stem:
--- src/data_engineering_sandbox/bson_to_sql/_transformations.py
+++ src/data_engineering_sandbox/bson_to_sql/_transformations.py
@@ -10,6 +10,23 @@ if TYPE_CHECKING:
 
 @define
 class NestedDataTransformer:
+    """Hold sqlalchemy table definition, relationship and data transformation for nested records.
+
+    Parameters
+    ----------
+    table_definition
+        Sqlalchemy table definition
+    transformation_fn
+        data transformation to apply on the extracted sub record
+    relationship_key
+        column name of the foreign key between the parent and child data points
+    data_source_key
+        if relationship_key must be added in the child data point,
+        we need the origin key from the parent to populate the child with a value.
+    add_relationship_in_nested
+        boolean to indicate if the foreign key is in the child data point or parent data point.
+    """
+
     table_definition: "DeclarativeAttributeIntercept"
     transformation_fn: "Callable[[Any], dict]" = field(default=lambda x: x)
     relationship_key: Optional[str] = field(default=None)
@@ -24,5 +41,6 @@ class NestedDataTransformer:
 
 
 def normalize_id(x: dict):
+    """Rename the key `_id` to `id`and cast it into string."""
     x["id"] = str(x.pop("_id"))
     return x
